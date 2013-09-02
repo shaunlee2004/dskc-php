feb3f76ec72e40703a128cf0eb2798774375cced
Shaun Lee <shaunlee2004@gmail.com>
2013-09-02 16:43:56 -0400

    better styling of dojo-kun

--- application/views/DojoRules-DojoKun.php
+++ application/views/DojoRules-DojoKun.php
@@ -6,32 +6,32 @@ Courtesy of Hiroyoshi Okazaki
 <br />
 
 <div style="float:left;">
-<ul class="unorderedList">
-	<li><span class="japaneseTerm">Hitotsu! Jinkaku kansei ni tsutomuru koto!</span>
-		<?php echo HTML::file_anchor("/media/audio/1_Seek_Perfection_of_Character.wav", "(listen)")?><br />
-		*Shtots! Jeenkaku kansay nee sto muru koto<br/>
-		First! Seek perfection of character!
-	</li>
-	<li><span class="japaneseTerm">Hitotsu! Makato no michi o mamoru koto!</span>
-		<?php echo HTML::file_anchor("/media/audio/2_Be_Faithful.wav", "(listen)")?><br />
-		*Shtots! Mahkoto no meechee o mahmoru koto<br/>
-		First! Be Faithful!
-	</li>
-	<li><span class="japaneseTerm">Hitosu! Doryoku no seishin o yashinau koto!</span>
-		<?php echo HTML::file_anchor("/media/audio/3_Endeavor.wav", "(listen)")?><br />
-		*Shtots! Do ryooku no saysheen o yashinow koto.<br/>
-		First! Endeavor
-	</li>
-	<li><span class="japaneseTerm">Hitosu! Reigi o omonzuru koto!</span>
-		<?php echo HTML::file_anchor("/media/audio/4_Respect_Others.wav", "(listen)")?><br />
-		*Shtots! Ray gi o omonzuru koto<br/>
-		First! Respect others!
-	</li>
-	<li><span class="japaneseTerm">Hitosu! Kekki no yu o imashimuru koto!</span>
-		<?php echo HTML::file_anchor("/media/audio/5_Refrain_from_Violent_Behavior.wav", "(listen)")?><br />
-		*Shtots! Keh key no you o imashimuru koto<br/>
-		First! Refrain from violent behavior!
-	</li>
-</ul>
+<dl>
+	<dt><span class="japaneseTerm">Hitotsu! Jinkaku kansei ni tsutomuru koto!</span>
+		<?php echo HTML::file_anchor("/media/audio/1_Seek_Perfection_of_Character.wav", "(listen)")?>
+		<dd>*Shtots! Jeenkaku kansay nee sto muru koto</dd>
+		<dd>First! Seek perfection of character!</dd>
+
+	<dt><span class="japaneseTerm">Hitotsu! Makato no michi o mamoru koto!</span>
+		<?php echo HTML::file_anchor("/media/audio/2_Be_Faithful.wav", "(listen)")?>
+		<dd>*Shtots! Mahkoto no meechee o mahmoru koto</dd>
+		<dd>First! Be Faithful!</dd>
+	</dt>
+	<dt><span class="japaneseTerm">Hitosu! Doryoku no seishin o yashinau koto!</span>
+		<?php echo HTML::file_anchor("/media/audio/3_Endeavor.wav", "(listen)")?>
+		<dd>*Shtots! Do ryooku no saysheen o yashinow koto.</dd>
+		<dd>First! Endeavor</dd>
+	</dt>
+	<dt><span class="japaneseTerm">Hitosu! Reigi o omonzuru koto!</span>
+		<?php echo HTML::file_anchor("/media/audio/4_Respect_Others.wav", "(listen)")?>
+		<dd>*Shtots! Ray gi o omonzuru koto</dd>
+		<dd>First! Respect others!</dd>
+	</dt>
+	<dt><span class="japaneseTerm">Hitosu! Kekki no yu o imashimuru koto!</span>
+		<?php echo HTML::file_anchor("/media/audio/5_Refrain_from_Violent_Behavior.wav", "(listen)")?>
+		<dd>*Shtots! Keh key no you o imashimuru koto</dd>
+		<dd>First! Refrain from violent behavior!</dd>
+	</dt>
+</dl>
 </div>
 
