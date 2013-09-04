2909609901eed48825cae742fa5a4c289ec8eec0
Shaun Lee <shaunlee2004@gmail.com>
2013-09-04 17:56:45 -0400

    yahoo-formatting stripped

--- application/views/dojoRules-events.php
+++ application/views/dojoRules-events.php
@@ -1,42 +1,26 @@
-<!--$sitebuilder version="2.8.5" extra="Java(1.7.0_09)" md5="995fe501c07250c9585964177e389731"$-->
-<!--$templateKey Blank||1.0.0$-->
-<html>
-  <head>
-    <title>Events</title>
-    <meta name="generator" content="Yahoo! SiteBuilder/2.8.5/1.7.0_09">
-    <meta name="description" content="Dominion Shotokan Karate Club, LLC calendar of upcoming events">
-    <meta name="keywords" content="Shotokan, karate, classes">
-    <!--$page size 1005, 3000$-->
-    <!--$page margin 0, 0, 20, 0$-->
-    <!--$centered$-->
-    <!--$fontFamily Arial$-->
-    <!--$fontSize 14$-->
-    <style type="text/css"><!--
-      BODY {font-family:"Arial"; font-size:14;margin:0px;padding:0px;text-align:center;min-width:1005px;}
-      P {font-family:"Arial"; font-size:14;}
-      FORM {margin:0;padding:0;}
-      #centerwrapper {text-align:left;width:1005px;margin-left:auto;margin-right:auto;}
-    --></style>
-  </head>
-  <body bgcolor="#253447" text="#000000" link="#0000FF" vlink="#800080" topmargin="0" leftmargin="0">
+<p>
+	The ISKF hosts several tournaments per year. Regional tournaments are
+	held in each of the ISKF districts around the country. Regional winners
+	and selected invitees are then eligible for the ISKF Nationals which
+	are hosted by a different region each year. The United States Shotokan
+	team is selected from the winners and exceptional participants
+	nominated by the tournament judges. Each tournament has events for
+	children, collegiate, senior and advanced ranks. Tournaments at Master
+	and Fall camps have events for adults of all ranks. It is strongly
+	encouraged that each member of the <span class="japaneseTerm">dojo</span>
+	participates in ISKF tournaments whenever possible.
+</p>
+<p>Master Camp and Fall Camp are held in June and September,
+	respectively. Master Camp welcomes Shotokan participants from around
+	the world to train for up to a week with some of the best instructors
+	from around the world. Fall Camp is held during a weekend and features
+	training by the most elite regional instructors available.</p>
+<p>
+	Many times during the year, senior instructors are invited to give
+	special trainings at local clubs. We offer special training events
+	prior to our <span class="japaneseTerm">kyu </span>exams and put this
+	information on our website so that neighboring clubs can attend as
+	well. There is usually a small fee required to compensate the
+	instructor and their expenses. Again, you are encouraged to attend.
+</p>
 
-<!--$begin exclude$-->
-    <div id="centerwrapper">
-      <div id="root" style="position:absolute;width:1005px;height:669px;">
-<!--$end exclude$-->
-        <div id="e0" style="position:absolute;left:1;top:0;width:998;height:78;"><!--$img %ImageAssetImpl:/images/generic_dusk_header.jpg$--><img src="sitebuilder/images/generic_dusk_header-998x78.jpg" width="998" height="78" alt=""></div>        <div id="e1" style="position:absolute;left:1;top:78;width:998;height:44;"><img src="images/generic_dusk_search_bar_background_tile.gif" width="998" height="44" alt=""></div>        <div id="e2" style="position:absolute;left:150;top:25;width:586;height:40;"><span class="text"><font color="#D4A84B" size="6"><span style="font-size:30px;line-height:35px;">Dojo Rules - Events<br/></span></font></span>        </div>
-        <div id="e3" style="position:absolute;left:155;top:175;width:675;height:250;"><span class="text"><font color="#979BA0" size="2"><span style="font-size:12px;line-height:15px;">The ISKF hosts several tournaments per year. Regional tournaments are held in each of the ISKF districts around the <br/>country. Regional winners and selected invitees are then eligible for the ISKF Nationals which are hosted by a different <br/>region each year. The United States Shotokan team is selected from the winners and exceptional participants nominated by <br/>the tournament judges. Each tournament has events for children, collegiate, senior and advanced ranks. Tournaments at <br/>Master and Fall camps have events for adults of all ranks. It is strongly encouraged that each member of the </span></font><i><font color="#979BA0" size="2"><span style="font-size:12px;line-height:15px;">dojo</span></font></i><font color="#979BA0" size="2"><span style="font-size:12px;line-height:15px;"> <br/>participates in ISKF tournaments whenever possible.<br><br>Master Camp and Fall Camp are held in June and September, respectively. Master Camp welcomes Shotokan participants <br/>from around the world to train for up to a week with some of the best instructors from around the world. Fall Camp is held <br/>during a weekend and features training by the most elite regional instructors available.<br><br>Many times during the year, senior instructors are invited to give special trainings at local clubs. We offer special training <br/>events prior to our </span></font><i><font color="#979BA0" size="2"><span style="font-size:12px;line-height:15px;">kyu </span></font></i><font color="#979BA0" size="2"><span style="font-size:12px;line-height:15px;">exams and put this information on our website so that neighboring clubs can attend as well. There is <br/>usually a small fee required to compensate the instructor and their expenses. Again, you are encouraged to attend.<br/></span></font></span>        </div>
-        <div id="e4" style="position:absolute;left:11;top:198;width:129;height:270;"><!--$navbar 
-name=navbar.nav
-assetID=%NavbarAsset:/navigation_bars/navbar.nav
-$--><!--$begin exclude$--><table border="0" cellspacing="0" cellpadding="0"><tr><td><a href="index.html"><img name="i5" src="sitebuilder/images/navbar-0-inactive-77129.png" border="0" width="129" height="30" alt=""/></a></td></tr><tr><td><a href="news.html"><img name="i6" src="sitebuilder/images/navbar-1-inactive-77285.png" border="0" width="129" height="30" alt=""/></a></td></tr><tr><td><a href="instructors.html" onMouseOver="document.images['i7'].src='sitebuilder/images/navbar-2-mouseOver-58579.png'" onMouseOut="document.images['i7'].src='sitebuilder/images/navbar-2-inactive-77348.png'"><img name="i7" src="sitebuilder/images/navbar-2-inactive-77348.png" border="0" width="129" height="30" alt=""/></a></td></tr><tr><td><a href="classes.html" onMouseOver="document.images['i8'].src='sitebuilder/images/navbar-3-mouseOver-58610.png'" onMouseOut="document.images['i8'].src='sitebuilder/images/navbar-3-inactive-77410.png'"><img name="i8" src="sitebuilder/images/navbar-3-inactive-77410.png" border="0" width="129" height="30" alt=""/></a></td></tr><tr><td><a href="DojoRules-Introduction.html" onMouseOver="document.images['i9'].src='sitebuilder/images/navbar-5-mouseOver-34128.png'" onMouseOut="document.images['i9'].src='sitebuilder/images/navbar-5-inactive-34097.png'"><img name="i9" src="sitebuilder/images/navbar-5-inactive-34097.png" border="0" width="129" height="30" alt=""/></a></td></tr><tr><td><a href="events.html" onMouseOver="document.images['i10'].src='sitebuilder/images/navbar-6-mouseOver-76292.png'" onMouseOut="document.images['i10'].src='sitebuilder/images/navbar-6-inactive-76261.png'"><img name="i10" src="sitebuilder/images/navbar-6-inactive-76261.png" border="0" width="129" height="30" alt=""/></a></td></tr><tr><td><a href="Photos.html" onMouseOver="document.images['i11'].src='sitebuilder/images/navbar-7-mouseOver-03618.png'" onMouseOut="document.images['i11'].src='sitebuilder/images/navbar-7-inactive-03586.png'"><img name="i11" src="sitebuilder/images/navbar-7-inactive-03586.png" border="0" width="129" height="30" alt=""/></a></td></tr><tr><td><a href="links.html" onMouseOver="document.images['i12'].src='sitebuilder/images/navbar-8-mouseOver-60165.png'" onMouseOut="document.images['i12'].src='sitebuilder/images/navbar-8-inactive-60149.png'"><img name="i12" src="sitebuilder/images/navbar-8-inactive-60149.png" border="0" width="129" height="30" alt=""/></a></td></tr><tr><td><a href="contactus.html" onMouseOver="document.images['i13'].src='sitebuilder/images/navbar-9-mouseOver-62733.png'" onMouseOut="document.images['i13'].src='sitebuilder/images/navbar-9-inactive-62714.png'"><img name="i13" src="sitebuilder/images/navbar-9-inactive-62714.png" border="0" width="129" height="30" alt=""/></a></td></tr></table><!--$end exclude$-->        </div>
-        <div id="e14" style="position:absolute;left:150;top:80;width:446;height:28;"><span class="text"><font color="#D4A84B" size="4"><span style="font-size:18px;line-height:22px;">Dominion Shotokan Karate Club, LLC<br/></span></font></span>        </div>
-        <div id="e15" style="position:absolute;left:1;top:0;width:125;height:124;"><img src="images/site_logo_-_125x125_-_transparent.gif" width="125" height="124" alt=""></div>        <div id="e16" style="position:absolute;left:7;top:605;width:998;height:44;"><img src="images/generic_dusk_footer.gif" width="998" height="44" alt=""></div>        <div id="e17" style="position:absolute;left:345;top:618;width:308;height:18;"><span class="text"><font color="#D4A84B" size="2"><span style="font-size:12px;line-height:15px;">Copyright &#169; Dominion Shotokan Karate Club, LLC 2010<br/></span></font></span>        </div>
-        <div id="e18" style="position:absolute;left:850;top:175;width:100;height:200;"><span class="text"><b><font color="#D4A84B" size="2"><span style="font-size:12px;line-height:15px;">Dojo Rules<br>Navigation<br><br></span></font></b><a href="DojoRules-Introduction.html"><font size="2"><span style="font-size:12px;line-height:15px;">01 </span></font></a><a href="DojoRules-Introduction.html"><font size="2"><span style="font-size:12px;line-height:15px;">I</span></font></a><a href="DojoRules-Introduction.html"><font size="2"><span style="font-size:12px;line-height:15px;">ntroduction<br></span></font></a><a href="DojoRules-FAQs.html"><font size="2"><span style="font-size:12px;line-height:15px;">02 FAQs<br></span></font></a><a href="DojoRules-Etiquette.html"><font size="2"><span style="font-size:12px;line-height:15px;">03 Etiquette<br></span></font></a><a href="DojoRules-Protocols.html"><font size="2"><span style="font-size:12px;line-height:15px;">04 Protocols<br></span></font></a><a href="DojoRules-Ranks.html"><font size="2"><span style="font-size:12px;line-height:15px;">05 Ranks<br></span></font></a><a href="DojoRules-Events.html"><font size="2"><span style="font-size:12px;line-height:15px;">06 Events<br></span></font></a><a href="DojoRules-Terminology.html"><font size="2"><span style="font-size:12px;line-height:15px;">07 Terminology<br></span></font></a><a href="DojoRules-ShotokanKata.html"><font size="2"><span style="font-size:12px;line-height:15px;">08 Kata<br></span></font></a><a href="DojoRules-DojoKun.html"><font size="2"><span style="font-size:12px;line-height:15px;">09 Dojo Kun<br></span></font></a><a href="DojoRules-NijuKun.html"><font size="2"><span style="font-size:12px;line-height:15px;">10 Niju Kun<br/></span></font></a></span>        </div>
-<!--$begin exclude$-->
-      </div>
-    </div>
-<!--$end exclude$-->
-<!--$begin exclude$--><div style="position:absolute;left:50;top:50;background:white"><font color=black>This file is not intended to be viewed directly using a web browser.  To create a viewable file, use the <b>Preview in Browser</b> or <b>Publish to Yahoo! Web Hosting</b> commands from within Yahoo! SiteBuilder.</font></div><!--$end exclude$-->
-  </body>
-</html>
--- application/views/dojoRules-introduction.php
+++ application/views/dojoRules-introduction.php
@@ -1,60 +1,37 @@
-These are rules and regulations for the Dominion Shotokan Karate Club,
-LLC (DSKC). They have been designed to help students feel more comfortable from the beginning and instill a basic
-knowledge of the culture, terminology and procedures that are required in a
-<span class="japaneseTerm">dojo</span>. It will require some time
-		and effort during your training to learn the proper procedures, rules
-		of <br />etiquette and aspects of Japanese culture. This is understood
-		as practicing karate is much different than most endeavors you
-		have undertaken in your life thus far. You will make mistakes; learn
-		from them. If you have questions, feel free to ask the <span class="japaneseTerm">sensei</span>or a <span class="japaneseTerm">senpai</span> at the proper time. We are
-		all here to help you in your journey.<br>
-	<br>We practice the Japanese art of Shotokan Karate and are affiliated
-		with the International Shotokan Karate Federation (ISKF) <br />headquartered
-		in Philadelphia, Pennsylvania. The chairman and chief instructor is
-		Master Teruyuki Okazaki, 10th Dan and a <br />direct student of the
-		founder of Shotokan, Master Gichin Funakoshi. All students are
-		required to become members of the <br />ISKF and can participate in
-		all ISKF tournaments, special trainings, Master Camps and Fall Camps.
-		In addition, students <br />who are traveling should be able to
-		practice at any ISKF
-</span></font>
-<i><font color="#979BA0" size="2"><span
-		style="font-size: 12px; line-height: 15px;"> dojo</span></font></i>
-<font color="#979BA0" size="2"><span
-	style="font-size: 12px; line-height: 15px;">, worldwide, for a nominal
-		fee. You will need to see your <br />instructor prior to your trip to
-		obtain a letter of introduction for the
-</span></font>
-<i><font color="#979BA0" size="2"><span
-		style="font-size: 12px; line-height: 15px;">sensei</span></font></i>
-<font color="#979BA0" size="2"><span
-	style="font-size: 12px; line-height: 15px;"> at the other </span></font>
-<i><font color="#979BA0" size="2"><span
-		style="font-size: 12px; line-height: 15px;">dojo</span></font></i>
-<font color="#979BA0" size="2"><span
-	style="font-size: 12px; line-height: 15px;">, to discuss the small
-		gift, </span></font>
-<i><font color="#979BA0" size="2"><span
-		style="font-size: 12px; line-height: 15px;">omiyage</span></font></i>
-<font color="#979BA0" size="2"><span
-	style="font-size: 12px; line-height: 15px;">, <br />you should bring to
-		the
-</span></font>
-<i><font color="#979BA0" size="2"><span
-		style="font-size: 12px; line-height: 15px;">sensei</span></font></i>
-<font color="#979BA0" size="2"><span
-	style="font-size: 12px; line-height: 15px;"> and about your expected
-		conduct. Remember, you are representing our </span></font>
-<i><font color="#979BA0" size="2"><span
-		style="font-size: 12px; line-height: 15px;">dojo</span></font></i>
-<font color="#979BA0" size="2"><span
-	style="font-size: 12px; line-height: 15px;">; follow all their <br />rules
-		and do your best during your training visit.<br>
-	<br></span></font>
-<i><font color="#979BA0" size="2"><span
-		style="font-size: 12px; line-height: 15px;">&#8220;The ultimate aim of
-			karate-do lies not in victory or defeat, but in the perfection of
-			character of the person who studies it.&#8221;<br>Master Gichin
-			Funakoshi<br />
-	</span></font></i>
-
+<p>
+	These are rules and regulations for the Dominion Shotokan Karate Club,
+	LLC (DSKC). They have been designed to help students feel more
+	comfortable from the beginning and instill a basic knowledge of the
+	culture, terminology and procedures that are required in a <span
+		class="japaneseTerm">dojo</span>. It will require some time and effort
+	during your training to learn the proper procedures, rules of etiquette
+	and aspects of Japanese culture. This is understood as practicing
+	karate is much different than most endeavors you have undertaken in
+	your life thus far. You will make mistakes; learn from them. If you
+	have questions, feel free to ask the <span class="japaneseTerm">sensei</span>or
+	a <span class="japaneseTerm">senpai</span> at the proper time. We are
+	all here to help you in your journey.
+</p>
+<p>
+	We practice the Japanese art of Shotokan Karate and are affiliated with
+	the International Shotokan Karate Federation (ISKF) headquartered in
+	Philadelphia, Pennsylvania. The chairman and chief instructor is Master
+	Teruyuki Okazaki, 10th Dan and a direct student of the founder of
+	Shotokan, Master Gichin Funakoshi. All students are required to become
+	members of the ISKF and can participate in all ISKF tournaments,
+	special trainings, Master Camps and Fall Camps. In addition, students
+	who are traveling should be able to practice at any ISKF <span
+		class="japaneseTerm"> dojo</span>, worldwide, for a nominal fee. You
+	will need to see your instructor prior to your trip to obtain a letter
+	of introduction for the <span class="japaneseTerm">sensei</span> at the
+	other <span class="japaneseTerm">dojo</span>, to discuss the small
+	gift, <span class="japaneseTerm">omiyage</span>, you should bring to
+	the <span class="japaneseTerm">sensei</span> and about your expected
+	conduct. Remember, you are representing our <span class="japaneseTerm">dojo</span>;
+	follow all their rules and do your best during your training visit.
+</p>
+<p>
+	<i> &#8220;The ultimate aim of karate-do lies not in victory or defeat,
+		but in the perfection of character of the person who studies
+		it.&#8221;</i><br>Master Gichin Funakoshi </span></font></i>
+</p>
