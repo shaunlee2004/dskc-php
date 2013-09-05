291f08576cbe5997eef091a35774f5805796e5b1
Shaun Lee <shaunlee2004@gmail.com>
2013-09-05 14:08:14 -0400

    yahoo-formatting stripped

--- application/views/news.php
+++ application/views/news.php
@@ -1,4 +1,30 @@
+<div class="title">Virginia Youth Invitational Tournament</div>
+<div class="subtitle">Newport News<br/>December 8, 2012</div>
+<p>The
+			first Virginia Youth Invitational Tournament was held in Newport News
+			on December 8, 2012. Dominion Shotokan Karate Club sent
+			20 students to support this event. The students did their dojo
+			proud  by winning eight first place, eight second place, and ten
+			third place awards. In addition, our kata team placed
+			first.</p>
+<div class="paddedImage" style="float: left;">
+	<?php
+	echo HTML::image ( "/media/img/fall2012_062-570x378.jpg", array (
+			'width' => 570,
+			'height' => 378 
+	) )?>
+	<div class="caption">Some
+			of the Dominion students who participated in the tournament</div>
+</div>
+<div class="clearfloat"></div>
+<div class="title">Baileys Community Center Clean-up Days</div>
+<div class="subtitle">April and October, 2012</div>
 
-        <div class="title">Baileys Community Center Clean-up Days</div>April and October, 2012<br/>
-        <div id="e20" style="position:absolute;left:83;top:805;width:482;height:264;"><!--$img %ImageAssetImpl:/images/Baileys_cleanup_with_sean.jpg$--><img src="sitebuilder/images/Baileys_cleanup_with_sean-482x264.jpg" width="482" height="264" alt=""></div>        <div id="e21" style="position:absolute;left:178;top:167;width:581;height:107;"><span class="text"><b><font color="#FF9900" size="4"><span style="font-size:20px;line-height:24px;">Virginia Youth Invitational Tournament - December 8, 2012<br><br></span></font></b><span style="font-size:12px;line-height:15px;">The first Virginia Youth Invitational Tournament was held in Newport News on December 8, 2012. &nbsp;<br/>Dominion Shotokan Karate Club sent 20 students to support this event. &nbsp;The students did their dojo proud <br/>by winning eight first place, eight second place, and ten third place awards. &nbsp;In addition, our kata team &nbsp;<br/>placed first.<br/></span></span>        </div>
-        <div id="e22" style="position:absolute;left:183;top:281;width:570;height:378;"><!--$img %ImageAssetImpl:/images/fall2012_062.JPG$--><img src="sitebuilder/images/fall2012_062-570x378.jpg" width="570" height="378" alt=""></div>        <div id="e23" style="position:absolute;left:206;top:668;width:543;height:35;"><span class="text"><span style="font-size:12px;line-height:15px;">Some of the Dominion students who participated in the tournament<br/></span></span>        </div>
+<div class="paddedImage" style="float: left;">
+	<?php
+	echo HTML::image ( "/media/img/Baileys_cleanup_with_sean-482x264.jpg", array (
+			'width' => 482,
+			'height' => 264 
+	) )?>
+	<div class="caption">Director Chon Lacey (back-left, red) leading cleanup efforts with students</div>
+</div>
