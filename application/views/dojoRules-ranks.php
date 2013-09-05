f3f78bbf131063ffc42c223c340a5da1ca165df1
Shaun Lee <shaunlee2004@gmail.com>
2013-09-04 20:26:27 -0400

    yahoo-formatting stripped
    think about rewording the 26-class minimum with quarterly classes

--- application/views/dojoRules-ranks.php
+++ application/views/dojoRules-ranks.php
@@ -1,42 +1,66 @@
-<!--$sitebuilder version="2.8.5" extra="Java(1.7.0_09)" md5="15681ac26ea93ee198f8be7c64fa3253"$-->
-<!--$templateKey Blank||1.0.0$-->
-<html>
-  <head>
-    <title>Events</title>
-    <meta name="generator" content="Yahoo! SiteBuilder/2.8.5/1.7.0_09">
-    <meta name="description" content="Dominion Shotokan Karate Club, LLC calendar of upcoming events">
-    <meta name="keywords" content="Shotokan, karate, classes">
-    <!--$page size 1005, 3000$-->
-    <!--$page margin 0, 0, 20, 0$-->
-    <!--$centered$-->
-    <!--$fontFamily Arial$-->
-    <!--$fontSize 14$-->
-    <style type="text/css"><!--
-      BODY {font-family:"Arial"; font-size:14;margin:0px;padding:0px;text-align:center;min-width:1005px;}
-      P {font-family:"Arial"; font-size:14;}
-      FORM {margin:0;padding:0;}
-      #centerwrapper {text-align:left;width:1005px;margin-left:auto;margin-right:auto;}
-    --></style>
-  </head>
-  <body bgcolor="#253447" text="#000000" link="#0000FF" vlink="#800080" topmargin="0" leftmargin="0">
+<p>
+	Originally, there were no belts in karate except those used to hold up
+	your pants. Master Jigoro Kano, the founder of <span
+		class="japaneseTerm">judo</span>, instituted a belt ranking necessary
+	to have an intermediate level (brown). Eventually, another color,
+	(green) was added between white and brown. This has now evolved into
+	the rainbow of colors we have currently. In Shotokan, the belts and
+	ranks are:
+</p>
 
-<!--$begin exclude$-->
-    <div id="centerwrapper">
-      <div id="root" style="position:absolute;width:1005px;height:615px;">
-<!--$end exclude$-->
-        <div id="e0" style="position:absolute;left:1;top:0;width:998;height:78;"><!--$img %ImageAssetImpl:/images/generic_dusk_header.jpg$--><img src="sitebuilder/images/generic_dusk_header-998x78.jpg" width="998" height="78" alt=""></div>        <div id="e1" style="position:absolute;left:1;top:78;width:998;height:44;"><img src="images/generic_dusk_search_bar_background_tile.gif" width="998" height="44" alt=""></div>        <div id="e2" style="position:absolute;left:150;top:25;width:260;height:40;"><span class="text"><font color="#D4A84B" size="6"><span style="font-size:30px;line-height:35px;">Dojo Rules - Ranks<br/></span></font></span>        </div>
-        <div id="e3" style="position:absolute;left:155;top:175;width:675;height:350;"><span class="text"><font color="#979BA0" size="2"><span style="font-size:12px;line-height:15px;">Originally, there were no belts in karate except those used to hold up your pants. Master Jigoro Kano, the founder of judo, <br/>instituted a belt ranking necessary to have an intermediate level (brown). Eventually, another color, (green) was added <br/>between white and brown. This has now evolved into the rainbow of colors we have currently. In Shotokan, the belts and <br/>ranks are:<br><br>White&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;.....10th </span></font><i><font color="#979BA0" size="2"><span style="font-size:12px;line-height:15px;">kyu<br></span></font></i><font color="#979BA0" size="2"><span style="font-size:12px;line-height:15px;">White w/ black stripe&#8230;..9th</span></font><i><font color="#979BA0" size="2"><span style="font-size:12px;line-height:15px;"> kyu<br></span></font></i><font color="#979BA0" size="2"><span style="font-size:12px;line-height:15px;">Yellow&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;...8th </span></font><i><font color="#979BA0" size="2"><span style="font-size:12px;line-height:15px;">kyu<br></span></font></i><font color="#979BA0" size="2"><span style="font-size:12px;line-height:15px;">Orange&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230; .7th </span></font><i><font color="#979BA0" size="2"><span style="font-size:12px;line-height:15px;">kyu<br></span></font></i><font color="#979BA0" size="2"><span style="font-size:12px;line-height:15px;">Green&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;. ..6th</span></font><i><font color="#979BA0" size="2"><span style="font-size:12px;line-height:15px;"> kyu<br></span></font></i><font color="#979BA0" size="2"><span style="font-size:12px;line-height:15px;">Purple&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;. .5th - 4th </span></font><i><font color="#979BA0" size="2"><span style="font-size:12px;line-height:15px;">kyu<br></span></font></i><font color="#979BA0" size="2"><span style="font-size:12px;line-height:15px;">Brown&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;. .3rd - 1st </span></font><i><font color="#979BA0" size="2"><span style="font-size:12px;line-height:15px;">kyu<br></span></font></i><font color="#979BA0" size="2"><span style="font-size:12px;line-height:15px;">Black&#8230;&#8230;&#8230;&#8230;&#8230;&#8230;&#8230; &#8230;1st - 10th </span></font><i><font color="#979BA0" size="2"><span style="font-size:12px;line-height:15px;">dan<br><br></span></font></i><font color="#979BA0" size="2"><span style="font-size:12px;line-height:15px;">White through orange are considered beginner ranks, green and purple are intermediate, and brown and black are <br/>advanced ranks. It will take, at minimum, four years training before you can apply to test for </span></font><i><font color="#979BA0" size="2"><span style="font-size:12px;line-height:15px;">dan</span></font></i><font color="#979BA0" size="2"><span style="font-size:12px;line-height:15px;"> rank or black belt.<br><br>Testing is an opportunity for students to advance in rank. Tests are scheduled three times per year, usually in January, May <br/>and September. Members must have accrued, at least, 26 classes before being allowed to test. This is an average of about <br/>two classes per week, which is the minimum number of classes someone can train. Students testing must pay an exam <br/>fee and, if membership has expired, a one year membership fee to the ISKF. There is no guarantee that you will pass. <br/>Remember, it is not about belts; it is about perfecting your character.<br/></span></font></span>        </div>
-        <div id="e4" style="position:absolute;left:11;top:198;width:129;height:270;"><!--$navbar 
-name=navbar.nav
-assetID=%NavbarAsset:/navigation_bars/navbar.nav
-$--><!--$begin exclude$--><table border="0" cellspacing="0" cellpadding="0"><tr><td><a href="index.html"><img name="i5" src="sitebuilder/images/navbar-0-inactive-77129.png" border="0" width="129" height="30" alt=""/></a></td></tr><tr><td><a href="news.html"><img name="i6" src="sitebuilder/images/navbar-1-inactive-77285.png" border="0" width="129" height="30" alt=""/></a></td></tr><tr><td><a href="instructors.html" onMouseOver="document.images['i7'].src='sitebuilder/images/navbar-2-mouseOver-58579.png'" onMouseOut="document.images['i7'].src='sitebuilder/images/navbar-2-inactive-77348.png'"><img name="i7" src="sitebuilder/images/navbar-2-inactive-77348.png" border="0" width="129" height="30" alt=""/></a></td></tr><tr><td><a href="classes.html" onMouseOver="document.images['i8'].src='sitebuilder/images/navbar-3-mouseOver-58610.png'" onMouseOut="document.images['i8'].src='sitebuilder/images/navbar-3-inactive-77410.png'"><img name="i8" src="sitebuilder/images/navbar-3-inactive-77410.png" border="0" width="129" height="30" alt=""/></a></td></tr><tr><td><a href="DojoRules-Introduction.html" onMouseOver="document.images['i9'].src='sitebuilder/images/navbar-5-mouseOver-34128.png'" onMouseOut="document.images['i9'].src='sitebuilder/images/navbar-5-inactive-34097.png'"><img name="i9" src="sitebuilder/images/navbar-5-inactive-34097.png" border="0" width="129" height="30" alt=""/></a></td></tr><tr><td><a href="events.html" onMouseOver="document.images['i10'].src='sitebuilder/images/navbar-6-mouseOver-76292.png'" onMouseOut="document.images['i10'].src='sitebuilder/images/navbar-6-inactive-76261.png'"><img name="i10" src="sitebuilder/images/navbar-6-inactive-76261.png" border="0" width="129" height="30" alt=""/></a></td></tr><tr><td><a href="Photos.html" onMouseOver="document.images['i11'].src='sitebuilder/images/navbar-7-mouseOver-03618.png'" onMouseOut="document.images['i11'].src='sitebuilder/images/navbar-7-inactive-03586.png'"><img name="i11" src="sitebuilder/images/navbar-7-inactive-03586.png" border="0" width="129" height="30" alt=""/></a></td></tr><tr><td><a href="links.html" onMouseOver="document.images['i12'].src='sitebuilder/images/navbar-8-mouseOver-60165.png'" onMouseOut="document.images['i12'].src='sitebuilder/images/navbar-8-inactive-60149.png'"><img name="i12" src="sitebuilder/images/navbar-8-inactive-60149.png" border="0" width="129" height="30" alt=""/></a></td></tr><tr><td><a href="contactus.html" onMouseOver="document.images['i13'].src='sitebuilder/images/navbar-9-mouseOver-62733.png'" onMouseOut="document.images['i13'].src='sitebuilder/images/navbar-9-inactive-62714.png'"><img name="i13" src="sitebuilder/images/navbar-9-inactive-62714.png" border="0" width="129" height="30" alt=""/></a></td></tr></table><!--$end exclude$-->        </div>
-        <div id="e14" style="position:absolute;left:150;top:80;width:446;height:28;"><span class="text"><font color="#D4A84B" size="4"><span style="font-size:18px;line-height:22px;">Dominion Shotokan Karate Club, LLC<br/></span></font></span>        </div>
-        <div id="e15" style="position:absolute;left:1;top:0;width:125;height:124;"><img src="images/site_logo_-_125x125_-_transparent.gif" width="125" height="124" alt=""></div>        <div id="e16" style="position:absolute;left:7;top:551;width:998;height:44;"><img src="images/generic_dusk_footer.gif" width="998" height="44" alt=""></div>        <div id="e17" style="position:absolute;left:345;top:564;width:308;height:18;"><span class="text"><font color="#D4A84B" size="2"><span style="font-size:12px;line-height:15px;">Copyright &#169; Dominion Shotokan Karate Club, LLC 2010<br/></span></font></span>        </div>
-        <div id="e18" style="position:absolute;left:850;top:175;width:100;height:200;"><span class="text"><b><font color="#D4A84B" size="2"><span style="font-size:12px;line-height:15px;">Dojo Rules <br/>Navigation<br><br></span></font></b><a href="DojoRules-Introduction.html"><font size="2"><span style="font-size:12px;line-height:15px;">01 </span></font></a><a href="DojoRules-Introduction.html"><font size="2"><span style="font-size:12px;line-height:15px;">I</span></font></a><a href="DojoRules-Introduction.html"><font size="2"><span style="font-size:12px;line-height:15px;">ntroduction<br></span></font></a><a href="DojoRules-FAQs.html"><font size="2"><span style="font-size:12px;line-height:15px;">02 FAQs<br></span></font></a><a href="DojoRules-Etiquette.html"><font size="2"><span style="font-size:12px;line-height:15px;">03 Etiquette<br></span></font></a><a href="DojoRules-Protocols.html"><font size="2"><span style="font-size:12px;line-height:15px;">04 Protocols<br></span></font></a><a href="DojoRules-Ranks.html"><font size="2"><span style="font-size:12px;line-height:15px;">05 Ranks<br></span></font></a><a href="DojoRules-Events.html"><font size="2"><span style="font-size:12px;line-height:15px;">06 Events<br></span></font></a><a href="DojoRules-Terminology.html"><font size="2"><span style="font-size:12px;line-height:15px;">07 Terminology<br></span></font></a><a href="DojoRules-ShotokanKata.html"><font size="2"><span style="font-size:12px;line-height:15px;">08 Kata<br></span></font></a><a href="DojoRules-DojoKun.html"><font size="2"><span style="font-size:12px;line-height:15px;">09 Dojo Kun<br></span></font></a><a href="DojoRules-NijuKun.html"><font size="2"><span style="font-size:12px;line-height:15px;">10 Niju Kun<br/></span></font></a></span>        </div>
-<!--$begin exclude$-->
-      </div>
-    </div>
-<!--$end exclude$-->
-<!--$begin exclude$--><div style="position:absolute;left:50;top:50;background:white"><font color=black>This file is not intended to be viewed directly using a web browser.  To create a viewable file, use the <b>Preview in Browser</b> or <b>Publish to Yahoo! Web Hosting</b> commands from within Yahoo! SiteBuilder.</font></div><!--$end exclude$-->
-  </body>
-</html>
+
+<table class="topAligned bordered" style="margin-left: auto; margin-right: auto;">
+	<tr>
+		<td>White</td>
+		<td>10th <span class="japaneseTerm">kyu</span></td>
+	</tr>
+
+	<tr>
+		<td>White w/ black stripe</td>
+		<td>9th <span class="japaneseTerm">kyu</span></td>
+	</tr>
+
+	<tr>
+		<td>Yellow</td>
+		<td>7th <span class="japaneseTerm">kyu</span></td>
+	</tr>
+
+	<tr>
+		<td>Orange</td>
+		<td>6th <span class="japaneseTerm">kyu</span></td>
+	</tr>
+
+	<tr>
+		<td>Green</td>
+		<td>6th <span class="japaneseTerm">kyu</span></td>
+	</tr>
+
+	<tr>
+		<td>Pruple</td>
+		<td>5th - 4th <span class="japaneseTerm">kyu</span></td>
+	</tr>
+
+	<tr>
+		<td>Brown</td>
+		<td>3rd - 1st <span class="japaneseTerm">kyu</span></td>
+	</tr>
+
+	<tr>
+		<td>Black</td>
+		<td>1st - 10th <span class="japaneseTerm">dan</span></td>
+	</tr>
+</table>
+<p>
+	White through orange are considered beginner ranks, green and purple
+	are intermediate, and brown and black are advanced ranks. It will take,
+	at minimum, four years training before you can apply to test for <span
+		class="japaneseTerm">dan</span> rank or black belt.
+</p>
+<p>Testing is an opportunity for students to advance in rank. Tests are
+	scheduled three times per year, usually in January, May and September.
+	Members must have accrued, at least, 26 classes before being allowed to
+	test. This is an average of about two classes per week, which is the
+	minimum number of classes someone can train. Students testing must pay
+	an exam fee and, if membership has expired, a one year membership fee
+	to the ISKF. There is no guarantee that you will pass. Remember, it is
+	not about belts; it is about perfecting your character.</p>
