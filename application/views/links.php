579244bc9381344a8e40a64b8e81de31d9728eef
Shaun Lee <shaunlee2004@gmail.com>
2013-09-05 02:55:32 -0400

    yahoo-formatting stripped

--- application/views/links.php
+++ application/views/links.php
@@ -1,3 +1,3 @@
 <p><?php echo HTML::anchor("http://www.iskf.com", "International Shotokan Karate Federation (ISKF)")?></p>
-<p><?php echo HTML::anchor("http://www.theshotokanway.com/", "The Shotokan Way)")?></p>
+<p><?php echo HTML::anchor("http://www.theshotokanway.com/", "The Shotokan Way") ?></p>
 
