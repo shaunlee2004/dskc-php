7af21aeae746d1a3f54bb95e7633685db5cb454a
Shaun Lee <shaunlee2004@gmail.com>
2013-09-06 13:35:14 -0400

    forced maps to be simple rather than satellite

--- application/views/classes.php
+++ application/views/classes.php
@@ -4,8 +4,11 @@
 	<tr>
 		<td>Bailey's Community Center<br /> 5920 Summers Lane<br> Falls
 			Church, VA 22041<br> (703) 931-7027<br /> <br /> <a
-			href="https://maps.google.com/maps?q=5920+Summers+Lane+Falls+Church,+VA+2204">
-				<?php echo HTML::image ( "/media/img/googleMaps.jpg" )?>
+			href="https://maps.google.com/maps?q=5920+Summers+Lane+Falls+Church,+VA+2204&t=m">
+			<?php echo HTML::image ( "/media/img/googleMaps.jpg", array (
+				'width' => 80,
+				'height' => 40 
+			))?>
 			</a>
 		</td>
 		<td>Monday, Wednesday and Friday<br> 4:00 pm to 6:00 pm<br> <br>
@@ -24,8 +27,11 @@
 	<tr>
 		<td>James Lee Community Center<br> 2855-A Annandale Road<br> Falls
 			Church, VA 22042<br> (703) 534-3387 x200/201<br /> <br /> <a
-			href="https://maps.google.com/maps?q=James+Lee+Community+Center+2855-A+Annandale+Road+Falls+Church,+VA+22042">
-				<?php echo HTML::image ( "/media/img/googleMaps.jpg" )?>
+			href="https://maps.google.com/maps?q=James+Lee+Community+Center+2855-A+Annandale+Road+Falls+Church,+VA+22042&t=m">
+				<?php echo HTML::image ( "/media/img/googleMaps.jpg", array (
+					'width' => 80,
+					'height' => 40 
+				))?>
 			</a>
 		</td>
 		<td>Tuesday and Thursday<br>4:00 pm to 6:00 pm (children)<br>6:00 pm
