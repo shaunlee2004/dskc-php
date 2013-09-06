1c7965468285c481c6b915439937b7e441c7a063
Shaun Lee <shaunlee2004@gmail.com>
2013-09-06 13:33:08 -0400

    content changes and added color for belts

--- application/views/dojoRules-ranks.php
+++ application/views/dojoRules-ranks.php
@@ -1,51 +1,64 @@
 <p>
 	Originally, there were no belts in karate except those used to hold up
 	your pants. Master Jigoro Kano, the founder of <span
-		class="japaneseTerm">judo</span>, instituted a belt ranking necessary
-	to have an intermediate level (brown). Eventually, another color,
-	(green) was added between white and brown. This has now evolved into
-	the rainbow of colors we have currently. In Shotokan, the belts and
+		class="japaneseTerm">judo</span>, instituted a belt ranking system consiting of white and black. Later it was deemed necessary
+	to have an intermediate level (brown). Eventually another color,
+	green, was added between white and brown. This has now evolved into
+	the spectrum of colors we have currently. In Shotokan, the belts and
 	ranks are:
 </p>
 
 
 <table class="topAligned bordered" style="margin-left: auto; margin-right: auto;">
 	<tr>
+		<td style="width: 80px; background-color: white;"></td>
 		<td>White</td>
 		<td>10th <span class="japaneseTerm">kyu</span></td>
 	</tr>
 
 	<tr>
+		<td style="width: 80px; background-color: white;">
+		<hr style="background:black; border:0; height:7px" /></hr></td>
 		<td>White w/ black stripe</td>
 		<td>9th <span class="japaneseTerm">kyu</span></td>
 	</tr>
 
 	<tr>
+		<td style="width: 80px; background-color: yellow;"></td>
 		<td>Yellow</td>
+		<td>8th <span class="japaneseTerm">kyu</span></td>
+	</tr>
+
+	<tr>
+		<td style="width: 80px; background-color: orange;"></td>
+	<td>Orange</td>
 		<td>7th <span class="japaneseTerm">kyu</span></td>
 	</tr>
 
 	<tr>
-		<td>Orange</td>
-		<td>6th <span class="japaneseTerm">kyu</span></td>
-	</tr>
-
-	<tr>
+			<td style="width: 80px; background-color: green;"></td>
+	
 		<td>Green</td>
 		<td>6th <span class="japaneseTerm">kyu</span></td>
 	</tr>
 
 	<tr>
-		<td>Pruple</td>
+			<td style="width: 80px; background-color: purple;"></td>
+	
+		<td>Purple</td>
 		<td>5th - 4th <span class="japaneseTerm">kyu</span></td>
 	</tr>
 
 	<tr>
+			<td style="width: 80px; background-color: #663333;"></td>
+	
 		<td>Brown</td>
 		<td>3rd - 1st <span class="japaneseTerm">kyu</span></td>
 	</tr>
 
 	<tr>
+			<td style="width: 80px; background-color: black;"></td>
+	
 		<td>Black</td>
 		<td>1st - 10th <span class="japaneseTerm">dan</span></td>
 	</tr>
@@ -57,9 +70,9 @@
 		class="japaneseTerm">dan</span> rank or black belt.
 </p>
 <p>Testing is an opportunity for students to advance in rank. Tests are
-	scheduled three times per year, usually in January, May and September.
-	Members must have accrued, at least, 26 classes before being allowed to
-	test. This is an average of about two classes per week, which is the
+	scheduled four times per year, usually in January, April, July, and October.
+	Members must have accrued, at least, 24 classes before being allowed to
+	test. This is an minimum of about two classes per week, which is the
 	minimum number of classes someone can train. Students testing must pay
 	an exam fee and, if membership has expired, a one year membership fee
 	to the ISKF. There is no guarantee that you will pass. Remember, it is
