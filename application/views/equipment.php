a7d2e00d74a52a41a3a8f663488a4fbab436668b
Shaun Lee <shaunlee2004@gmail.com>
2013-09-06 17:53:42 -0400

    spruced up pictures, belts need to be corrected for shotokan kyus

--- application/views/equipment.php
+++ application/views/equipment.php
@@ -1,35 +1,51 @@
-<div class="paddedImage" style="float: left; position: relative;">
+
+	<div class="paddedImage" style="float: left; position: relative;">
 	<?php
-	echo HTML::image ( "/media/img/white_gloves.jpg") ?>
-	<div class="caption">
-		White vinyl sparring gloves
+	echo HTML::image ( "/media/img/ISKF_patch.jpg" )?>
+	<div class="caption">ISKF patch</div>
 	</div>
-</div>
-<div class="paddedImage" style="float: left; position: relative;">
+	<div class="paddedImage" style="float: left; position: relative;">
 	<?php
-	echo HTML::image ( "/media/img/ISKF_patch.jpg") ?>
-	<div class="caption">
-		ISKF patch
+	echo HTML::image ( "/media/img/white_gloves.jpg", array (
+			'height' => 182,
+			'width' => 200 
+	) )?>
+	<div class="caption">White vinyl sparring gloves</div>
 	</div>
-</div>
-<div class="paddedImage" style="float: left; position: relative;">
+	<div class="paddedImage" style="float: left; position: relative;">
 	<?php
-	echo HTML::image ( "/media/img/karate_gi_youth.jpg", array(
-	'height' => 160,
-	'width' => 213
-)) ?>
-	<div class="caption">
-		Youth/Beginner Karate Uniform (gi)
+	echo HTML::image ( "/media/img/tokaido_karate_mouthguard.jpg", array (
+			'height' => 200,
+			'width' => 223 
+	) )?>
+	<div class="caption">Mouthguard</div>
 	</div>
-</div>
-<div class="clearfloat"></div>
-<div class="paddedImage" style="float: right; position: relative;">
-	<?php
-	echo HTML::image ( "/media/img/tokaido-karate-mouth-guard.jpg", array(
-	'height' => 200,
-	'width' =>200
-)) ?>
-	<div class="caption">
-		Mouthguard
+<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+	<div class="paddedImage" style="float: left; position: relative;">
+		<?php
+		echo HTML::image ( "/media/img/belts.jpg", array (
+				'height' => 180,
+				'width' => 354 
+		) )?>
+	<div class="caption">belts</div>
+
 	</div>
-</div>
+	<div class="paddedImage" style="float: left; position: relative;">
+		<?php
+		echo HTML::image ( "/media/img/karate_gi_youth.jpg", array (
+				'height' => 160,
+				'width' => 213 
+		) )?>
+	<div class="caption">Youth/Beginner Karate Uniform (gi)</div>
+
+	</div>
+
+	<div class="paddedImage" style="float: left; position: relative;">
+		<?php
+		echo HTML::image ( "/media/img/karate_gi_adult.jpg", array (
+				'height' => 200,
+				'width' => 200 
+		) )?>
+	<div class="caption">Adult/Advanced Karate Uniform (gi)</div>
+
+	</div>
