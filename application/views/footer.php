6ed18be208006f0b591b982f77098715925f4d3f
Shaun Lee <shaunlee2004@gmail.com>
2013-09-06 17:11:34 -0400

    site version in footer
    move the value of site version to a config file in the future

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,3 +1,4 @@
 <div id="footer">
-	Copyright &#169; Dominion Shotokan Karate Club, LLC 2010-2013
+	Copyright &#169; Dominion Shotokan Karate Club, LLC 2010-2013<br/>
+	Site Version: 2.00.01 (09-Sept-2013)
 </div>
