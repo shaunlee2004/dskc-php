38e1ff84800b1eb66cac6528988a7cddd1365e6f
Shaun Lee <shaunlee2004@gmail.com>
2013-09-06 13:32:24 -0400

    added content (kyu exam, nationals) and cloned text from dojorules-events

--- application/views/events.php
+++ application/views/events.php
@@ -1,30 +1,54 @@
 <div style="float: left; padding: 5px;">
 	<p>
 		East Coast Fall Camp<br /> 26th Anniversary<br /> Sept 13-15th, 2013<br />
-				<?php echo HTML::anchor("https://maps.google.com/maps?daddr=249+Camp+Green+Lane+Rd,+Green+Lane,+PA", "Camp Green Lane, PA")?><br />
+				<?php echo HTML::anchor("https://maps.google.com/maps?daddr=249+Camp+Green+Lane+Rd,+Green+Lane,+PA&t=m", "Camp Green Lane, PA")?><br />
 		<?php echo HTML::anchor("http://www.iskf.com/images/2013%20EC%20FC%20BROCHUA.pdf", "Registration Packet")?>
+				</p>
+	<p>
+	
+		<p>
+		Kyu exam<br /> Oct 19, 2013<br />
+				<?php echo HTML::anchor("https://maps.google.com/maps?q=5920+Summers+Lane+Falls+Church,+VA+2204&t=m", "Bailey&#39;s Community Center")?>
+				</p>
+	<p>
+	
+	
+	<p>
+		National Tournament<br /> 35th Anniversary<br /> Nov 16-17th, 2013<br />
+				<?php echo HTML::anchor("https://maps.google.com/maps?q=151+West+Fifth+Street+Cincinnati,+Ohio+45202&t=m", "Hyatt Regency Cincinatti")?><br />
+		<?php echo HTML::anchor("http://midamericaiskf.com/2013ISKFNationals.htm", "Information")?>
 				</p>
 	<p>
 <?php echo HTML::anchor("http://www.iskf.com/iskf%20headquarters.html", "Other ISKF Events")?>
 </p>
 
 </div>
-<div >
-	<div class="title">Tournaments/Camps/Clinics</div>
-	<p>the country. Regional winners and selected invitees are then
-		eligible for the ISKF Nationals which are hosted by a participants
-		nominated by the tournament judges. Each tournament has events for
-		children, collegiate, senior and advanced ranks. Tournaments at Master
-		and Fall camps have events for adults of all ranks. It is strongly
-		Master Camp and Fall Camp are held in June and September,
-		respectively. Master Camp welcomes Shotokan participants from around
-		the world to train for up to a week with some of the best instructors
-		from around the world. Fall Camp is held during a weekend and features
-		training by the most elite regional instructors available.</p>
-	<p>Many times during the year, senior instructors are invited to give
-		special trainings at local clubs. We offer special training events
-		prior to our exams and put this information on our website so that
-		neighboring clubs can attend as well. There is usually a small fee
-		required to compensate the instructor and their expenses. Again, you
-		are encouraged to attend.</p>
-</div>
+<div class="title">Tournaments</div>
+<p>
+	The ISKF hosts several tournaments per year. Regional tournaments are
+	held in each of the ISKF districts around the country. Regional winners
+	and selected invitees are then eligible for the ISKF Nationals which
+	are hosted by a different region each year. The United States Shotokan
+	team is selected from the winners and exceptional participants
+	nominated by the tournament judges. Each tournament has events for
+	children, collegiate, senior and advanced ranks. Tournaments at Master
+	and Fall camps have events for adults of all ranks. It is strongly
+	encouraged that each member of the <span class="japaneseTerm">dojo</span>
+	participates in ISKF tournaments whenever possible.
+</p>
+<div class="title">Camps</div>
+
+<p>Master and Fall Camps are held at Camp Green Lane, PA in June and September,
+	respectively. Master Camp welcomes Shotokan participants from around
+	the world to train for up to a week with some of the world's most reknown instructors. Fall Camp is held over a weekend and features
+	training by the most elite regional instructors.</p>
+<div class="title">Seminars</div>
+	
+	<p>
+	Several times during the year, senior instructors are invited to give
+	special trainings at local clubs. We offer special training events
+	prior to some <span class="japaneseTerm">kyu </span>exams and put this
+	information on our website so that neighboring clubs can attend as
+	well. There is usually a small fee required to compensate the
+	instructor and their expenses. Again, you are strongly encouraged to attend.
+</p>
