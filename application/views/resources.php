1b6616e418ab9ed8200bc16e7012a7000e70970d
Shaun Lee <shaunlee2004@gmail.com>
2013-09-08 23:53:07 -0400

    rearranged page images

--- application/views/resources.php
+++ application/views/resources.php
@@ -1,68 +1,93 @@
-<div style="float: left">
 
-	<dl>
-		<dt>
-			<div class="title">Sites</div>
-		</dt>
-		<dt>
+<div class="title">Sites</div>
+<dl>
+	<dt>
 	<?php echo HTML::anchor("http://www.iskf.com", "International Shotokan Karate Federation (ISKF)")?></dt>
-		<dt><?php echo HTML::anchor("http://www.theshotokanway.com/", "The Shotokan Way") ?></dt>
-	</dl>
-	<dl>
-		<dt>
-			<div class="title">Files</div>
-		</dt>
+	<dt><?php echo HTML::anchor("http://www.theshotokanway.com/", "The Shotokan Way") ?></dt>
+</dl>
 
-		<dt><?php echo HTML::file_anchor("/media/files/Shotokan_PocketMod.pdf", "Pocket Guide to Terminology")  ?> (courtesy of Liane Rozzell)</dt>
-		<dd><?php echo HTML::anchor("http://pocketmod.com/", "folding instructions") ?></dd>
-	</dl>
-	<dl>
-		<dt>
-			<div class="title">Recommended Reading</div>
-		</dt>
-		<dt>Best Karate #1-11 by Masatoshi Nakayama</dt>
-		<dd>
+<div class="title">Files</div>
+<dl>
+	<dt><?php echo HTML::file_anchor("/media/files/Shotokan_PocketMod.pdf", "Pocket Guide to Terminology")  ?> (courtesy of Liane Rozzell)</dt>
+	<dd><?php echo HTML::anchor("http://pocketmod.com/", "folding instructions") ?></dd>
+</dl>
+<div class="title">Recommended Reading</div>
+
+<div style="float: left; position: relative; padding: 5px;">
+	<div class="subtitle">Ginchin Funakoshi</div>
+	Karate-D&#333: My Way of Life<br /> The Twenty Guiding Principle of
+	Karate<br /> Karate-D&#333 Ky&#333han: The Master Text<br />
+			<?php echo HTML::image("/media/img/book_karate_do_my_way_of_life.jpg", array('height'=> 215, 'width' => 127))?>
+			<?php echo HTML::image("/media/img/book_karate_do_kyohan.jpg", array('height'=> 215, 'width' => 153))?>
+			<?php echo HTML::image("/media/img/book_twenty_guiding_principles_of_karate.jpg")?>
+	</div>
+<div class="clearfloat"></div>
+
+
+<div style="float: left; position: relative; padding: 5px;">
+	<div class="subtitle">Masatoshi Nakayama</div>
+	Best Karate #1-11<br /> Dynamic Karate<br />
+	<div style="float: left; position: relative;">
 		<?php echo HTML::image("/media/img/book_best_karate_1.jpg", array('height'=> 120, 'width' => 80))?>
 		<?php echo HTML::image("/media/img/book_best_karate_2.jpg", array('height'=> 120, 'width' => 80))?>
 		<?php echo HTML::image("/media/img/book_best_karate_3.jpg", array('height'=> 120, 'width' => 80))?>
 		<?php echo HTML::image("/media/img/book_best_karate_4.jpg", array('height'=> 120, 'width' => 80))?>
 		<?php echo HTML::image("/media/img/book_best_karate_5.jpg", array('height'=> 120, 'width' => 80))?>
-		<br/>
+		<br />
 		<?php echo HTML::image("/media/img/book_best_karate_6.jpg", array('height'=> 120, 'width' => 80))?>
 		<?php echo HTML::image("/media/img/book_best_karate_7.jpg", array('height'=> 120, 'width' => 80))?>
 		<?php echo HTML::image("/media/img/book_best_karate_8.jpg", array('height'=> 120, 'width' => 80))?>
 		<?php echo HTML::image("/media/img/book_best_karate_9.jpg", array('height'=> 120, 'width' => 80))?>
 		<?php echo HTML::image("/media/img/book_best_karate_10.jpg", array('height'=> 120, 'width' => 80))?>
 		<?php echo HTML::image("/media/img/book_best_karate_11.jpg", array('height'=> 120, 'width' => 80))?>
-		</dd>
-		<dt>Karate-Do: My Way of Life by Funakoshi</dt>
-		<dd><?php echo HTML::image("/media/img/book_karate_do_my_way_of_life.jpg", array('height'=> 215, 'width' => 127))?></dd>
-		<dt>Karate-D&#333 Ky&#333han: The Master Text by Ginchin Funakoshi</dt>
-		<dd><?php echo HTML::image("/media/img/book_karate_do_kyohan.jpg", array('height'=> 215, 'width' => 153))?></dd>
-		<dt>The Twenty Guiding Principle of Karate by Ginchin Funakoshi</dt>
-		<dd><?php echo HTML::image("/media/img/book_twenty_guiding_principles_of_karate.jpg")?></dd>
-		<dt>Dynamic Karate by Masatoshi Nakayama</dt><dd>
-		<dd><?php echo HTML::image("/media/img/book_dynamic_karate.jpg", array('height'=> 215, 'width' => 150))?></dd>
-		<dt>Textbook of Modern Karate by Teruyuki Okazaki</dt>
-		<dd><?php echo HTML::image("/media/img/book_textbook_of_modern_karate.jpg", array('height'=> 215, 'width' => 160))?></dd>
-		<dt>Perfection of Character: Guiding Principles for the Martial Arts & Everyday Life by Teruyuki Okazaki</dt>
-		<dd><?php echo HTML::image("/media/img/book_perfection_of_character.jpg", array('height'=> 215, 'width' => 156))?></dd>
-		<dt>Mind and body - Like Bullet by Yutaka Yaguchi</dt>
-		<dd><?php echo HTML::image("/media/img/book_mind_and_body_like_bullet.jpg", array('height'=> 215, 'width' => 157))?></dd>
-		<dt>Karate: The art of &quot;Empty-Hand&quot; Fighting by Hidetaka Nishiyama</dt>
-		<dd><?php echo HTML::image("/media/img/book_karate_the_art_of_empty_hand_fighting.jpg", array('height'=> 215, 'width' => 146))?></dd>
-		<dt>Complete Shotokan Karate: The Samurai Legacy and the Modern Practice by Robin L. Reilly</dt>
-		<dd><?php echo HTML::image("/media/img/book_complete_shotokan_karate.jpg", array('height'=> 215, 'width' => 136))?></dd>
-		<dt>Secrets of Shotokan Karate by Robin L. Reilly</dt>
-		<dd><?php echo HTML::image("/media/img/book_secrets_of_shotokan_karate.jpg")?></dd>
-		<dt>Karate Basics by Robin L. Reilly</dt>
-		<dd><?php echo HTML::image("/media/img/book_karate_basics.jpg", array('height'=> 215, 'width' => 142))?></dd>
-		<dt>The Book of Five Rings by Miyamoto Musashi</dt>
-		<dd><?php echo HTML::image("/media/img/book_the_book_of_five_rings.jpg", array('height'=> 215, 'width' => 143))?></dd>
-		<dt>The Unfettered Mind by Takuan S&#333;h&#333;</dt>
-		<dd><?php echo HTML::image("/media/img/book_the_unfettered_mind.jpg", array('height'=> 215, 'width' => 150))?></dd>
-		
-	</dl>
+	</div>
+	<?php echo HTML::image("/media/img/book_dynamic_karate.jpg", array('height'=> 244, 'width' => 171))?>
+</div>
+<div class="clearfloat"></div>
+
+<div style="float: left; position: relative; padding: 5px;">
+	<div class="subtitle">Teruyuki Okazaki</div>
+	Textbook of Modern Karate<br /> Perfection of Character:<br />&nbsp;&nbsp;&nbsp;Guiding
+	Principles for the Martial Arts &amp; Everyday Life<br /> Masterclass
+	Shotokan (DVD-set)<br />
+	<?php echo HTML::image("/media/img/book_textbook_of_modern_karate.jpg", array('height'=> 215, 'width' => 160))?>
+	<?php echo HTML::image("/media/img/book_perfection_of_character.jpg", array('height'=> 215, 'width' => 156))?>
+	<?php echo HTML::image("/media/img/dvd_masterclass_shotokan.gif", array('height'=> 215, 'width' => 407))?>
+	
+	</div>
+<div style="float: left; position: relative; padding: 5px;">
+<br/><br/><br/>
+	<div class="subtitle">Yutaka Yaguchi</div>
+	Mind and body - Like Bullet<br />
+	<?php echo HTML::image("/media/img/book_mind_and_body_like_bullet.jpg", array('height'=> 215, 'width' => 157))?>
+</div>
+<div class="clearfloat"></div>
+
+
+<div style="float: left; position: relative; padding: 5px;">
+<br/><br/>
+<div class="subtitle">Hidetaka Nishiyama</div>
+	Karate: The art of<br/>&nbsp;&nbsp;&nbsp;&quot;Empty-Hand&quot; Fighting<br />
+	<?php echo HTML::image("/media/img/book_karate_the_art_of_empty_hand_fighting.jpg", array('height'=> 215, 'width' => 146))?>
+</div>
+<div style="float: left; position: relative; padding: 5px;">
+	<div class="subtitle">Robin L. Reilly</div>
+	Complete Shotokan Karate:<br/>&nbsp;&nbsp;&nbsp;The Samurai Legacy and the Modern Practice<br />
+	Secrets of Shotokan Karate<br /> Karate Basics<br />
+	<?php echo HTML::image("/media/img/book_complete_shotokan_karate.jpg", array('height'=> 215, 'width' => 136))?>
+	<?php echo HTML::image("/media/img/book_secrets_of_shotokan_karate.jpg", array('height'=> 215, 'width' => 142))?>
+	<?php echo HTML::image("/media/img/book_karate_basics.jpg", array('height'=> 215, 'width' => 142))?>
 </div>
 
-
+<div style="float: left; position: relative; padding: 5px;">
+<br/><br/><br/>
+<div class="subtitle">Miyamoto Musashi</div>
+	The Book of Five Rings<br />
+<?php echo HTML::image("/media/img/book_the_book_of_five_rings.jpg", array('height'=> 215, 'width' => 143))?>
+</div>
+<div style="float: left; position: relative; padding: 5px;">
+<br/><br/><br/>
+<div class="subtitle">Takuan S&#333;h&#333;</div>
+	The Unfettered Mind<br />
+<?php echo HTML::image("/media/img/book_the_unfettered_mind.jpg", array('height'=> 215, 'width' => 150))?>
+</div>
