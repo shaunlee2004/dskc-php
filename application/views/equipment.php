541ae1af8c295bf936a9baafb889e6999018cdd5
Shaun Lee <shaunlee2004@gmail.com>
2013-09-12 17:21:42 -0400

    arranged images

--- application/views/equipment.php
+++ application/views/equipment.php
@@ -1,48 +1,46 @@
 
-	<div class="paddedImage" style="float: left; position: relative;">
+<div class="paddedImage" style="float: left; position: relative;">
 	<?php
 	echo HTML::image ( "/media/img/ISKF_patch.jpg" )?>
 	<div class="caption">ISKF patch</div>
-	</div>
-	<div class="paddedImage" style="float: left; position: relative;">
+</div>
+<div class="paddedImage" style="float: left; position: relative;">
 	<?php
-	echo HTML::image ( "/media/img/white_gloves.jpg", array (
-			'height' => 182,
-			'width' => 200 
-	) )?>
+	echo HTML::image ( "/media/img/white_gloves.jpg" )?>
 	<div class="caption">White vinyl sparring gloves</div>
-	</div>
-	<div class="paddedImage" style="float: left; position: relative;">
+</div>
+<div class="paddedImage" style="float: left; position: relative;">
 	<?php
 	echo HTML::image ( "/media/img/tokaido_karate_mouthguard.jpg", array (
 			'height' => 200,
 			'width' => 223 
 	) )?>
 	<div class="caption">Mouthguard</div>
-	</div>
-<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-	<div class="paddedImage" style="float: left; position: relative;">
-		<?php
-		echo HTML::image ( "/media/img/karate_belts.jpg" )?>
-	<div class="caption">belts</div>
+</div>
+<div style="float: left; position: relative;">
 
-	</div>
 	<div class="paddedImage" style="float: left; position: relative;">
+			<?php
+			echo HTML::image ( "/media/img/karate_belts.jpg" )?>
+		<div class="caption">belts</div>
+	</div>
+<div class="clearfloat"></div>
+	<div class="paddedImage" style="float: left; position: relative;">
+			<?php
+			echo HTML::image ( "/media/img/karate_gi_adult.jpg", array (
+					'height' => 300,
+					'width' => 300 
+			) )?>
+			<div class="caption">Adult/Advanced Karate Uniform (gi)</div>
+	</div>
+
+</div>
+<div class="paddedImage" style="float: left; position: relative;">
 		<?php
 		echo HTML::image ( "/media/img/karate_gi_youth.jpg", array (
-				'height' => 160,
-				'width' => 213 
+				'height' => 195,
+				'width' => 260 
 		) )?>
 	<div class="caption">Youth/Beginner Karate Uniform (gi)</div>
+</div>
 
-	</div>
-
-	<div class="paddedImage" style="float: left; position: relative;">
-		<?php
-		echo HTML::image ( "/media/img/karate_gi_adult.jpg", array (
-				'height' => 200,
-				'width' => 200 
-		) )?>
-	<div class="caption">Adult/Advanced Karate Uniform (gi)</div>
-
-	</div>
