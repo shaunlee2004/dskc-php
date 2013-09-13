a6cc2da731c565d382115ac9990d19c8b5715586
Shaun Lee <shaunlee2004@gmail.com>
2013-09-13 01:44:36 -0400

    search engine optimization
    http://www.metatags.org/meta_tags_code_generator

--- application/views/template/template.php
+++ application/views/template/template.php
@@ -3,10 +3,19 @@
 <head>
 <meta charset="utf-8">
 <title><?php echo $title ?></title>
-      <?php foreach ($styles as $file => $type) echo HTML::style($file, array('media' => $type)), PHP_EOL ?>
+      <?php foreach ($styles as $file => $type) echo HTML::style($file, array('media' => $type)), PHP_EOL?>
           <?php foreach ($scripts as $file) echo HTML::script($file), PHP_EOL?>
-    </head>
+<!-- made by www.metatags.org -->
+<meta name="description"
+	content="Dominion Shotokan Karate Club, LLC is a member of the ISKF, an international organization that seeks to promulgate traditional Japanese karate-do throughout the world." />
+<meta name="keywords"
+	content="Karate,Shotokan,Martial arts,training,Northern virginia, virginia, ISKF, International Shotokan Karate Federation, Teruyuki Okazaki, Steven Majors,, Steve Majors, Shaun Lee" />
+<meta name="author" content="Shaun Lee">
+<meta name="robots" content="index, follow">
+<meta name="revisit-after" content="1 month">
+<!-- Shotokan, Karate, Virginia -->
+</head>
 <body>
-                <?php echo $content?>
-      </body>
+	<?php echo $content?>
+</body>
 </html>
