9c7c192b98f7c073d80dfc260a99192f0dd3804b
Shaun Lee <shaunlee2004@gmail.com>
2013-09-13 02:12:33 -0400

    corrected test config file

--- system/config/dskc-test.php
+++ system/config/dskc-test.php
@@ -4,10 +4,10 @@ defined ( 'SYSPATH' ) or die ( 'No direct script access.' );
 
 return array (
 		'environment' => strtoupper('TEST'),
-		'base_url' => 'test.dominionshotokan.com',
+		'base_url' => 'http://test.dominionshotokan.com',
 		'version' => array (
 				'name' => '2.00.03',
-				'released' => '09/12/2013',
+				'released' => '09/13/2013',
 				'title' =>  'grammer and google'
 		), 
 );
