bdf9f157ddedb6a32a767b41799359e2aa6c7ac6
Shaun Lee <shaunlee2004@gmail.com>
2013-09-13 02:47:18 -0400

    Prod 2.00.03 (20130913) re-release config file

--- system/config/dskc-prod.php
+++ system/config/dskc-prod.php
@@ -4,10 +4,11 @@ defined ( 'SYSPATH' ) or die ( 'No direct script access.' );
 
 return array (
 		'environment' => strtoupper('PROD'),
-		'base_url' => 'dominionshotokan.com',
+		'base_url' => 'http://dominionshotokan.com',
 		'version' => array (
-				'name' => '2.00.02',
-				'released' => '09/12/2013' 
+				'name' => '2.00.03',
+				'released' => '09/13/2013', 
+				'title' =>  're-release'
 		), 
 );
 ?>
