0921848c88fb419ef624593d5918fda869173d43
Shaun Lee <shaunlee2004@gmail.com>
2013-10-02 01:36:09 -0400

    fall festival 2013 news update

--- application/views/news.php
+++ application/views/news.php
@@ -1,20 +1,41 @@
+<div class="title">Fall Festival</div>
+<div class="subtitle">
+	James Lee Community Center<br />Falls Church, VA<br />September 28,
+	2013
+</div>
+<p>
+	The James Lee Community Center held it's second annual Fall Festival to
+	showcase the services in the area. Many students came to support the
+	club and even translating for prospectives. They day ended with a
+	showcase of <span class="japaneseTerm">kata Heian Godan</span>, a
+	demonstration of <span class="japaneseTerm">bunkai</span> (<span
+		class="japaneseTerm">kata</span> interpretation), and some <span
+		class="japaneseTerm">kumite</span> (sparring)
+</p>
+
+<div class="paddedImage" style="float: left;">
+	<?php
+	echo HTML::image ( "/media/img/james_lee_fall_festival_2013.jpg" )?>
+	<div class="caption">karate-ka in action</div>
+</div>
+
 <div class="title">Virginia Youth Invitational Tournament</div>
-<div class="subtitle">Newport News<br/>December 8, 2012</div>
-<p>The
-			first Virginia Youth Invitational Tournament was held in Newport News
-			on December 8, 2012. Dominion Shotokan Karate Club sent
-			20 students to support this event. The students did their dojo
-			proud  by winning eight first place, eight second place, and ten
-			third place awards. In addition, our kata team placed
-			first.</p>
+<div class="subtitle">
+	Newport News<br />December 8, 2012
+</div>
+<p>The first Virginia Youth Invitational Tournament was held in Newport
+	News on December 8, 2012. Dominion Shotokan Karate Club sent 20
+	students to support this event. The students did their dojo proud by
+	winning eight first place, eight second place, and ten third place
+	awards. In addition, our kata team placed first.</p>
 <div class="paddedImage" style="float: left;">
 	<?php
 	echo HTML::image ( "/media/img/fall2012_062-570x378.jpg", array (
 			'width' => 570,
 			'height' => 378 
 	) )?>
-	<div class="caption">Some
-			of the Dominion students who participated in the tournament</div>
+	<div class="caption">Some of the Dominion students who participated in
+		the tournament</div>
 </div>
 <div class="clearfloat"></div>
 <div class="title">Baileys Community Center Clean-up Days</div>
@@ -26,5 +47,6 @@
 			'width' => 482,
 			'height' => 264 
 	) )?>
-	<div class="caption">Director Chon Lacey (back-left, red) leading cleanup efforts with students</div>
+	<div class="caption">Director Chon Lacey (back-left, red) leading
+		cleanup efforts with students</div>
 </div>
