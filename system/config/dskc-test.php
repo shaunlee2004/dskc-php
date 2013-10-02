f9449ce74dcdefee29f749ad5fa524212b91c99e
Shaun Lee <shaunlee2004@gmail.com>
2013-10-02 01:37:44 -0400

    config update for 2.00.04

--- system/config/dskc-test.php
+++ system/config/dskc-test.php
@@ -6,8 +6,8 @@ return array (
 		'environment' => strtoupper('TEST'),
 		'base_url' => 'test.dominionshotokan.com',
 		'version' => array (
-				'name' => '2.00.02',
-				'released' => '09/12/2013' 
+				'name' => '2.00.04',
+				'released' => '10/02/2013' 
 		), 
 );
 ?>
