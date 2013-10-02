49bb48bf1e16ba51123af7d3f3bd7389147cabbb
Shaun Lee <shaunlee2004@gmail.com>
2013-10-02 01:15:55 -0400

    more books and ISBN-13 linked to amazon
    enoda, jose fraguas, kanazawa, and master camp

--- application/views/resources.php
+++ application/views/resources.php
@@ -18,18 +18,36 @@
 
 <div style="float: left; position: relative; padding: 5px;">
 	<div class="subtitle">Ginchin Funakoshi</div>
-	Karate-D&#333: My Way of Life<br /> The Twenty Guiding Principle of
-	Karate<br /> Karate-D&#333 Ky&#333han: The Master Text<br />
+	Karate-D&#333: My Way of Life
+	<?php echo HTML::anchor('http://www.amazon.com/Karate-Do-Way-Life-Gichin-Funakoshi/dp/1568364989', 'ISBN: 978-1568364988')?><br />
+	The Twenty Guiding Principle of
+	Karate
+	<?php echo HTML::anchor('http://www.amazon.com/The-Twenty-Guiding-Principles-Karate/dp/1568364962', 'ISBN: 978-1568364964')?><br />
+	Karate-D&#333 Ky&#333han: The Master Text
+	<?php echo HTML::anchor('http://www.amazon.com/Karate-Do-Kyohan-The-Master-Text/dp/1568364822', 'ISBN: 978-1568364827')?><br />
 			<?php echo HTML::image("/media/img/book_karate_do_my_way_of_life.jpg", array('height'=> 215, 'width' => 127))?>
-			<?php echo HTML::image("/media/img/book_karate_do_kyohan.jpg", array('height'=> 215, 'width' => 153))?>
 			<?php echo HTML::image("/media/img/book_twenty_guiding_principles_of_karate.jpg")?>
+			<?php echo HTML::image("/media/img/book_karate_do_kyohan.jpg", array('height'=> 215, 'width' => 153))?>
 	</div>
 <div class="clearfloat"></div>
 
 
 <div style="float: left; position: relative; padding: 5px;">
 	<div class="subtitle">Masatoshi Nakayama</div>
-	Best Karate #1-11<br /> Dynamic Karate<br />
+	Best Karate #1-11<br />
+	&nbsp;&nbsp;&nbsp;&nbsp;<?php echo HTML::anchor('http://www.amazon.com/Best-Karate-Vol-1-Masatoshi-Nakayama/dp/1568364636/', '#1 ISBN: 978-1568364636')?>
+	&nbsp;&nbsp;&nbsp;&nbsp;<?php echo HTML::anchor('http://www.amazon.com/Best-Karate-Vol-2-Masatoshi-Nakayama/dp/1568364644/', '#2 ISBN: 978-1568364643')?>
+	&nbsp;&nbsp;&nbsp;&nbsp;<?php echo HTML::anchor('http://www.amazon.com/Best-Karate-Vol-3-Kumite-1/dp/0870113321/', '#3 ISBN: 978-0870113321')?><br/>
+	&nbsp;&nbsp;&nbsp;&nbsp;<?php echo HTML::anchor('http://www.amazon.com/Best-Karate-Vol-4-Kumite-2/dp/1568364652/', '#4 ISBN: 978-1568364650')?>
+	&nbsp;&nbsp;&nbsp;&nbsp;<?php echo HTML::anchor('http://www.amazon.com/Best-Karate-Vol-5-Heian-Tekki/dp/1568364725/', '#5 ISBN: 978-1568364728')?>
+	&nbsp;&nbsp;&nbsp;&nbsp;<?php echo HTML::anchor('http://www.amazon.com/Best-Karate-Bassai-Kanku-Vol/dp/0870113836/', '#6 ISBN: 978-0870113833')?><br/>
+	&nbsp;&nbsp;&nbsp;&nbsp;<?php echo HTML::anchor('http://www.amazon.com/Best-Karate-Vol-7-Jutte-Hangetsu/dp/1568364660/', '#7 ISBN: 978-1568364667')?>
+	&nbsp;&nbsp;&nbsp;&nbsp;<?php echo HTML::anchor('http://www.amazon.com/Best-Karate-Vol-8-Gankaku-Jion/dp/1568364679/', '#8 ISBN: 978-1568364674')?>
+	&nbsp;&nbsp;&nbsp;&nbsp;<?php echo HTML::anchor('http://www.amazon.com/Best-Karate-Vol-9-Bassai-Chinte/dp/1568364687/', '#9 ISBN: 978-1568364681')?><br/>
+	&nbsp;&nbsp;&nbsp;&nbsp;<?php echo HTML::anchor('http://www.amazon.com/Best-Karate-Vol-10-Nijushiho-International/dp/0870117343/', '#10 ISBN: 978-0870117343')?>
+	&nbsp;&nbsp;&nbsp;&nbsp;<?php echo HTML::anchor('http://www.amazon.com/Best-Karate-Vol-11-Gojushiho-Meikyo/dp/0870117580/', '#11 ISBN: 978-0870117589')?><br/>
+	Dynamic Karate
+	<?php echo HTML::anchor('http://www.amazon.com/Dynamic-Karate-Masatoshi-Nakayama/dp/156836413X/', 'ISBN: 978-1568364131')?><br/>
 	<div style="float: left; position: relative;">
 		<?php echo HTML::image("/media/img/book_best_karate_1.jpg", array('height'=> 120, 'width' => 80))?>
 		<?php echo HTML::image("/media/img/book_best_karate_2.jpg", array('height'=> 120, 'width' => 80))?>
@@ -50,9 +68,14 @@
 
 <div style="float: left; position: relative; padding: 5px;">
 	<div class="subtitle">Teruyuki Okazaki</div>
-	Textbook of Modern Karate<br /> Perfection of Character:<br />&nbsp;&nbsp;&nbsp;Guiding
-	Principles for the Martial Arts &amp; Everyday Life<br /> Masterclass
-	Shotokan (DVD-set)<br />
+	Textbook of Modern Karate 
+	<?php echo HTML::anchor('http://www.amazon.com/Textbook-Modern-Karate-Teruyuki-Okazaki/dp/0870114611', 'ISBN: 978-0870114618')?><br />
+	Perfection of Character: Guiding
+	Principles for the Martial Arts &amp; Everyday Life
+	<?php echo HTML::anchor('http://www.amazon.com/Perfection-Character-Guiding-Principles-Everyday/dp/0978576322', 'ISBN: 978-0978576325')?>
+	<br /> Masterclass
+	Shotokan (DVD-set)
+	<?php echo HTML::anchor('http://www.em3video.com/MasterClassDVDs.html', 'ISBN: 978-1606610206')?><br />
 	<?php echo HTML::image("/media/img/book_textbook_of_modern_karate.jpg", array('height'=> 215, 'width' => 160))?>
 	<?php echo HTML::image("/media/img/book_perfection_of_character.jpg", array('height'=> 215, 'width' => 156))?>
 	<?php echo HTML::image("/media/img/dvd_masterclass_shotokan.gif", array('height'=> 215, 'width' => 407))?>
@@ -62,35 +85,100 @@
 <br/><br/><br/>
 	<div class="subtitle">Yutaka Yaguchi</div>
 	Mind and body - Like Bullet<br />
+	&nbsp;&nbsp;&nbsp;&nbsp;<?php echo HTML::anchor('http://www.yaguchikarate.com/buy-now.html', 'ISBN: 978-1578334001')?><br/>
 	<?php echo HTML::image("/media/img/book_mind_and_body_like_bullet.jpg", array('height'=> 215, 'width' => 157))?>
 </div>
-<div class="clearfloat"></div>
 
+<div style="float: left; position: relative; padding: 5px;">
+	<div class="subtitle">Various</div>
+	Shotokan Masters: ISKF Master Camp Training (DVD-set)<br />
+	&nbsp;&nbsp;&nbsp;featuring: Teruyuki Okazaki, Hirokazu Kanazawa<br />
+	&nbsp;&nbsp;&nbsp;Yutaka Yaguchi, Hideo Ochi, Shunsuke Takahashi,<br />
+	&nbsp;&nbsp;&nbsp;and Masaru Miura<br/>
+	&nbsp;&nbsp;&nbsp;<?php echo HTML::anchor('http://www.budovideos.com/shop/customer/product.php?productid=31428', 'purchase')?><br/>
+	<?php echo HTML::image("/media/img/dvd_shotokan_masters_iskf_master_camp_training.jpg", array('height'=> 215, 'width' => 152))?>
+</div>
 
 <div style="float: left; position: relative; padding: 5px;">
 <br/><br/>
 <div class="subtitle">Hidetaka Nishiyama</div>
 	Karate: The art of<br/>&nbsp;&nbsp;&nbsp;&quot;Empty-Hand&quot; Fighting<br />
+	<?php echo HTML::anchor('http://www.amazon.com/Karate-Empty-Hand-Fighting-Hidetaka-Nishiyama/dp/0804816689', 'ISBN: 978-0804816687')?><br/>
 	<?php echo HTML::image("/media/img/book_karate_the_art_of_empty_hand_fighting.jpg", array('height'=> 215, 'width' => 146))?>
 </div>
+
+
+<div style="float: left; position: relative; padding: 5px;">
+<div class="subtitle">Hirokazu Kanazawa</div>
+	Karate: The Complete Kata <?php echo HTML::anchor('http://www.amazon.com/Karate-Complete-Kata-Hirokazu-Kanazawa/dp/1568365179/', 'ISBN: 978-1568365176')?><br />
+	Karate Fighting Techniques <?php echo HTML::anchor('http://www.amazon.com/Karate-Fighting-Techniques-Complete-Kumite/dp/1568365160/', 'ISBN: 978-1568365169')?><br/>
+<?php echo HTML::image("/media/img/book_karate_the_complete_kata.jpg", array('height'=> 215, 'width' => 157))?>
+<?php echo HTML::image("/media/img/book_karate_fighting_techniques.jpg", array('height'=> 215, 'width' => 158))?>
+</div>
+
+<div style="float: left; position: relative; padding: 5px;">
+<div class="subtitle">Keinosuke Enoeda</div>
+	Shotokan Advanced Kata: Volume 1
+	<?php echo HTML::anchor('http://www.amazon.com/Shotokan-Advanced-Kata-Vol-1/dp/094606203X/', 'ISBN: 978-0946062034')?><br />
+	Shotokan Advanced Kata: Volume 2
+	<?php echo HTML::anchor('http://www.amazon.com/Shotokan-Advanced-Kata-v-2/dp/0946062048/', 'ISBN: 978-0946062041')?><br />
+	Shotokan Advanced Kata: Volume 3
+	<?php echo HTML::anchor('http://www.amazon.com/Shotokan-Advanced-Kata-Vol-3/dp/0946062056/', 'ISBN: 978-0946062058;')?><br />
+	Tiger of Shotokan Karate
+	<?php echo HTML::anchor('http://www.amazon.com/Keinosuke-Enoeda-Tiger-Shotokan-Karate/dp/0954694716/', 'ISBN: 978-0954694715')?><br/>	
+<?php echo HTML::image("/media/img/book_shotokan_advanced_kata_1.jpg", array('height'=> 215, 'width' => 153))?>
+<?php echo HTML::image("/media/img/book_shotokan_advanced_kata_2.jpg", array('height'=> 215, 'width' => 152))?>
+<?php echo HTML::image("/media/img/book_shotokan_advanced_kata_3.jpg", array('height'=> 215, 'width' => 154))?>
+<?php echo HTML::image("/media/img/book_tiger_of_shotokan_karate.jpg", array('height'=> 215, 'width' => 168))?>
+</div>
+
 <div style="float: left; position: relative; padding: 5px;">
 	<div class="subtitle">Robin L. Reilly</div>
-	Complete Shotokan Karate:<br/>&nbsp;&nbsp;&nbsp;The Samurai Legacy and the Modern Practice<br />
-	Secrets of Shotokan Karate<br /> Karate Basics<br />
+	Complete Shotokan Karate: The Samurai Legacy and the Modern Practice
+	<?php echo HTML::anchor('http://www.amazon.com/Complete-Shotokan-Karate-Philosophy-Practice/dp/0804821089/', 'ISBN: 978-0804821087')?><br />
+	The Secrets of Shotokan Karate
+	<?php echo HTML::anchor('http://www.amazon.com/Secrets-Shotokan-Karate-Robin-Rielly/dp/0804832293/', 'ISBN: 978-0804832298')?><br />
+	Karate Basics
+	<?php echo HTML::anchor('http://www.amazon.com/Karate-Basics-Tuttle-Martial-Arts/dp/0804834938/', 'ISBN: 978-0804834933')?><br />
 	<?php echo HTML::image("/media/img/book_complete_shotokan_karate.jpg", array('height'=> 215, 'width' => 136))?>
 	<?php echo HTML::image("/media/img/book_secrets_of_shotokan_karate.jpg", array('height'=> 215, 'width' => 142))?>
 	<?php echo HTML::image("/media/img/book_karate_basics.jpg", array('height'=> 215, 'width' => 142))?>
 </div>
 
 <div style="float: left; position: relative; padding: 5px;">
+<div class="subtitle">Jose Fraguas</div>
+	Karate Masters: Volume 1
+	<?php echo HTML::anchor('http://www.amazon.com/Karate-Masters-Jose-Fraguas/dp/0865681880/', 'ISBN: 978-0865681880')?><br/>
+	&nbsp;&nbsp;&nbsp;&nbsp;*Teruyuki Okazaki<br />
+	Karate Masters: Volume 2
+	<?php echo HTML::anchor('http://www.amazon.com/Karate-Masters-Jose-M-Fraguas/dp/1933901209/', 'ISBN: 978-1933901206')?><br />
+	Karate Masters: Volume 3
+	<?php echo HTML::anchor('http://www.amazon.com/Karate-Masters-Jose-Fraguas/dp/1933901047/', 'ISBN: 978-1933901046')?><br />
+	Karate Masters: Volume 4
+	<?php echo HTML::anchor('http://www.amazon.com/Karate-Masters-Jose-M-Fraguas/dp/1933901497/', 'ISBN: 978-1933901497')?><br/>
+	&nbsp;&nbsp;&nbsp;&nbsp;*Yutaka Yaguchi and Hiroyoshi Okazaki<br />
+<?php echo HTML::image("/media/img/book_karate_masters_1.jpg", array('height'=> 215, 'width' => 149))?>
+<?php echo HTML::image("/media/img/book_karate_masters_2.jpg", array('height'=> 215, 'width' => 149))?>
+<?php echo HTML::image("/media/img/book_karate_masters_3.jpg", array('height'=> 215, 'width' => 149))?>
+<?php echo HTML::image("/media/img/book_karate_masters_4.jpg", array('height'=> 215, 'width' => 151))?>
+</div>
+
+<div class="clearfloat"></div>
+
+<div style="float: left; position: relative; padding: 5px;">
 <br/><br/><br/>
 <div class="subtitle">Miyamoto Musashi</div>
 	The Book of Five Rings<br />
+	&nbsp;&nbsp;&nbsp;&nbsp;<?php echo HTML::anchor('http://www.amazon.com/Book-Five-Rings-Miyamoto-Musashi/dp/1590309847/', 'ISBN: 978-1590309841')?><br/>
 <?php echo HTML::image("/media/img/book_the_book_of_five_rings.jpg", array('height'=> 215, 'width' => 143))?>
 </div>
+
 <div style="float: left; position: relative; padding: 5px;">
 <br/><br/><br/>
 <div class="subtitle">Takuan S&#333;h&#333;</div>
 	The Unfettered Mind<br />
+	&nbsp;&nbsp;&nbsp;&nbsp;<?php echo HTML::anchor('http://www.amazon.com/The-Unfettered-Mind-Writings-Swordsman/dp/1590309863/', 'ISBN: 978-1590309865')?><br/>
 <?php echo HTML::image("/media/img/book_the_unfettered_mind.jpg", array('height'=> 215, 'width' => 150))?>
 </div>
+
+
