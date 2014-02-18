2e89653c52022d4f76da20338bd9e46bdf60e369
Shaun Lee <shaunlee2004@gmail.com>
2014-02-18 00:34:27 -0500

    2.00.12 configs after test env confirmation

--- system/config/dskc-dev.php
+++ system/config/dskc-dev.php
@@ -6,8 +6,8 @@ return array (
 		'environment' => strtoupper('DEV'),
 		'base_url' => '',
 		'version' => array (
-				'name' => '2.00.11',
-				'released' => '02/14/2014' 
+				'name' => '2.00.12',
+				'released' => '02/18/2014' 
 		), 
 		'calendar' => array(
 				'url' => 'dominionshotokan@gmail.com',
--- system/config/dskc-test.php
+++ system/config/dskc-test.php
@@ -7,7 +7,7 @@ return array (
 		'base_url' => 'http://test.dominionshotokan.com',
 		'version' => array (
 				'name' => '2.00.12',
-				'released' => '02/17/2014' 
+				'released' => '02/18/2014' 
 		), 
 		'calendar' => array(
 				'url' => 'dominionshotokan@gmail.com',
